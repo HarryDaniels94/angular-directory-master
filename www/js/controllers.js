d71f0ae30f25d7f16ff9d67c9261a7f758e664b3
HarryDaniels94 <harrydaniels@live.co.uk>
2015-04-02 15:49:50 +0100

    m

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -21,7 +21,7 @@ angular.module('myApp.controllers', [])
     .controller('ReportListCtrl', ['$scope', '$routeParams', 'Report', function ($scope, $routeParams, Report) {
         $scope.employees = Report.query({employeeId: $routeParams.employeeId});
     }])
-    .controller('ngStorageCtrl', function($scope, $localStorage) {
+    /*.controller('ngStorageCtrl', function($scope, $localStorage) {
         $scope.save = function(checkValue, employeeId) {
             if (checkValue === "Attending") {
 
@@ -38,4 +38,4 @@ angular.module('myApp.controllers', [])
         }
 
 
-    });
+    })*/;
